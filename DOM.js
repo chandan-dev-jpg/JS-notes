f79e08d1f05b771f3e8ef61d81b8ee859a597125
Array in java <chandanbehera4598@gmail.com>
2026-06-18 21:22:58 +0530

    Dom

--- DOM.js
+++ DOM.js
@@ -1,10 +1,5 @@
 // "The DOM (Document Object Model) is a tree-like representation of an HTML document that enables JavaScript to access and manipulate web page elements dynamically."
 
-let list = document.getElementById("list");
-
-list.innerHTML = `
-<li>Mango</li>
-<li>Banana</li>
-<li>Orange</li>
-`;
-console.log(list)
+let link=document.getElementById('link');
+link.setAttribute("href","https://google.com");
+link.setAttribute("target","_black");
