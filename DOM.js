8cfe0bb204ea3e8b201aa1141df805eca5849bf1
Array in java <chandanbehera4598@gmail.com>
2026-06-21 16:18:46 +0530

    Dom

--- DOM.js
+++ DOM.js
@@ -1,9 +1,17 @@
 // "The DOM (Document Object Model) is a tree-like representation of an HTML document that enables JavaScript to access and manipulate web page elements dynamically."
 
-let btn = document.getElementById("themeBtn");
-let box = document.getElementById("box");
+let btn=document.getElementById('Btn');
 
-btn.addEventListener("click", function () {
-    box.classList.add("dark");
-    console.log(box)
-});
+function showAlert()
+{
+alert("Button Clicked");
+}
+
+btn.addEventListener("click",showAlert);
+
+ // Remove click event after 3 seconds
+    setTimeout(() => {
+        btn.removeEventListener("click", showAlert);
+        console.log("Click event removed");
+    }, 3000);
+    console.log(btn)
